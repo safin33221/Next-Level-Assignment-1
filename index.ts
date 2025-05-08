c2ebd7f1535c9be58f16595484f1952a6df09ab7
safin33221 <safin33221@gmail.com>
2025-05-08 11:37:51 +0600

    New Function:  Completed filterByRating functionality.

--- index.ts
+++ index.ts
@@ -8,3 +8,20 @@ function formatString(input: string, toUpper?: boolean): string {
 // console.log(formatString("Hello",false));         
 // console.log(formatString("Hello", true));;  
 // console.log(formatString("Hello", false));  
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating > 4)
+}
+
+// const books = [
+//     { title: "Book A", rating: 4.5 },
+//     { title: "Book B", rating: 3.2 },
+//     { title: "Book C", rating: 5.0 }
+// ];
+
+// const result = filterByRating(books);
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+// console.log(result);
+
+
