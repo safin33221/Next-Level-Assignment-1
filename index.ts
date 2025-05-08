4a0fa762d4986cc3d855dee690ca3bfde4c0f0ad
safin33221 <safin33221@gmail.com>
2025-05-08 18:08:00 +0600

    Add: getMostExpensiveProduct and getDayType functions; implement squareAsync for async squaring

--- index.ts
+++ index.ts
@@ -76,5 +76,64 @@ function processValue(value: string | number): number {
 
 }
 
-console.log(processValue("hello"));
-console.log(processValue(10)    );
+// console.log(processValue("hello"));
+// console.log(processValue(10)    );
+
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) return null
+
+    return products.reduce((max, product) =>
+        product.price > max.price ? product : max
+    )
+}
+
+
+// const products = [
+//     { name: "Pen", price: 10 },
+//     { name: "Notebook", price: 25 },
+//     { name: "Bag", price: 50 }
+//   ];
+
+//   console.log(getMostExpensiveProduct(products));
+
+
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+function getDayType(day: Day): string {
+    if (day === Day.Saturday || day === Day.Sunday) {
+        return 'Weekend'
+    }
+    return 'Weekday'
+}
+
+
+// console.log(getDayType(Day.Monday));
+// console.log(getDayType(Day.Sunday));
+
+async function squareAsync(n: number): Promise<number> {
+    if (n < 0) {
+        throw new Error(' Nagitive number are not allowed')
+    }
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(n * n)
+        }, 1000);
+    })
+}
+
+squareAsync(-4).then(console.log);         // Output after 1s: 16
+squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
